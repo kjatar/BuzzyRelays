ca9a1ef57fdf69c854b09a26ce6c4a737c8bea7b
kjatar <kjatar@users.noreply.github.com>
2025-07-13 00:20:48 -0400

    Update BuzzyRelays.py

--- BuzzyRelays.py
+++ BuzzyRelays.py
@@ -3,6 +3,7 @@ import serial
 import sys
 from time import sleep
 import os
+import random
 
 ser = serial.Serial()
 ser.baudrate = 9600
@@ -14,19 +15,41 @@ ser.xonxoff = False     #disable software flow control
 ser.rtscts = False     #disable hardware (RTS/CTS) flow control
 ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
 ser.writeTimeout = 2     #timeout for write
+ser.port = "/dev/ttyUSB0"
+random.seed()
+ser.open()
+
+relayTriggers = [
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 30 46 46 30 30 46 45 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 30 30 30 30 30 46 44 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 31 46 46 30 30 46 44 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 31 30 30 30 30 46 43 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 32 46 46 30 30 46 43 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 32 30 30 30 30 46 42 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 33 46 46 30 30 46 42 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 33 30 30 30 30 46 41 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 34 46 46 30 30 46 41 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 34 30 30 30 30 46 39 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 35 46 46 30 30 46 39 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 35 30 30 30 30 46 38 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 36 46 46 30 30 46 38 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 36 30 30 30 30 46 37 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 37 46 46 30 30 46 37 0D 0A"),
+    bytearray.fromhex("3A 46 45 30 35 30 30 30 37 30 30 30 30 46 36 0D 0A")]
+    
 
 
 if len(sys.argv) != 2:
-    print ("Usage: python MyRelays.py <relay #>")
+    print ("Usage: python BuzzyRelays.py <relay #>")
     sys.exit()
 
 if sys.argv[1] == "TEST":
     for i in range(1, 8):
-        os.system("python MyRelays.py "+str(i))
+        os.system("python BuzzyRelays.py "+str(i))
 
 
 if sys.argv[1] == "1":
-    ser.port = "/dev/ttyUSB0"
+    
     ser.open()
     ser.write(bytearray.fromhex("3A 46 45 30 35 30 30 30 30 46 46 30 30 46 45 0D 0A"))
     sleep(0.25)
@@ -81,3 +104,8 @@ if sys.argv[1] == "8":
     sleep(0.25)
     ser.write(bytearray.fromhex("3A 46 45 30 35 30 30 30 37 30 30 30 30 46 36 0D 0A"))
 
+
+if sys.arg[1] == "RANDOM":
+    for i in range(1, 20):
+        ser.write(relayTriggers[random.randint(1,16)])
+        sleep(10)
